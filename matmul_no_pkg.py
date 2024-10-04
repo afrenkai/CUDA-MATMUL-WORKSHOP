ca10f0744121f0db87069db13d9de237e5bc8054
Artem <afrenk@wpi.edu>
2024-10-04 10:21:54 -0400

    main

--- matmul_no_pkg.py
+++ matmul_no_pkg.py
@@ -1,6 +1,25 @@
 from typing import List
 Matrix = List[List[int]]
-def MatMul(A: Matrix, B: Matrix, m: int, n: int, p: int) -> List[List[int]]:
+
+def is_matrix(matrix: Matrix): bool:
+    if not matrix or not matrix[0]:
+        return False
+
+    row_len = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_len:
+            return False
+
+    return True
+
+
+def MatMul(A: Matrix, B: Matrix) -> List[List[int]]:
+    if not is_matrix(A) or not is_matrix(B):
+        raise ValueError("One or More of the Matrices is not properly sized")
+    m = len(A) # n rows in A
+    n = len(A[0]) # n cols in A and rows in B
+    p = len(B[0]) # n cols in B
+
     C = []
     for x in range (m):
         row = []
@@ -19,4 +38,31 @@ def MatMul(A: Matrix, B: Matrix, m: int, n: int, p: int) -> List[List[int]]:
     return C
 
 A = [[0, 2, 3, 9, 7, 3], [7, 0, 5, 6, 5, 4], [4, 2, 8, 7, 0, 0],[1, 4, 6, 8, 3, 7], [7, 4, 7, 5, 1, 8], [3, 8, 5, 8, 9, 4]]
-print(eval(A, Matrix))
+B = [[0, 9, 3, 2, 6, 5], [5, 4, 9, 5, 1, 6]]
+def run(A, B) -> None:
+    # Checking A
+
+    if is_matrix(A):
+        print("A is a valid matrix")
+    else:
+        print("A is not a valid matrix")
+
+
+    #Checking B
+
+
+    if is_matrix(B):
+        print('B is a valid Matrix')
+    else:
+        print('B is not a valid Matrix')
+
+
+    if is_matrix(A) and is_matrix(B):
+        res = MatMul(A,B)
+        print(res)
+    else:
+        print("One of the matrices is invalid, check the prior console output")
+
+
+if name = "__main__":
+    run(A, B)
